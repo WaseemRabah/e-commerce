4e684d34c8602372dca66f0bdff271c862237c78
waseem <waseem.96.ra@gmail.com>
2023-11-13 14:05:44 +0100

    Register models and add products database

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='media/')
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
