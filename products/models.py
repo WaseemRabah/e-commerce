39014c6556620a0acb35c005abd593936159558c
waseem <waseem.96.ra@gmail.com>
2023-11-16 00:53:56 +0100

    Add sorting and product counts to product page

--- products/models.py
+++ products/models.py
@@ -18,6 +18,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
